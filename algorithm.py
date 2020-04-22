ee4a8e903e7fd23c9ac1f9e200adce334c1cd295
Charlton <thenewtechwiz@gmail.com>
2020-04-22 13:25:55 -0700

    Completely Random algorithm (Works up to 6 times)

--- algorithm.py
+++ algorithm.py
@@ -13,15 +13,17 @@ class Node:
         return self.name
 
 
-A = Node('A')
-B = Node('B')
-C = Node('C')
-D = Node('D')
-E = Node('E')
-F = Node('F')
-G = Node('G')
-H = Node('H')
-I = Node('I')
+class Output:
+    def __init__(self):
+        self.output = ""
+
+
+# config
+number_of_people_in_a_group = 7
+number_of_groups = 15
+number_of_rounds = 6
+total_people = 105
+
 
 def generate_score(lst1, lst2, node, current_node_visited):
     if node in current_node_visited:
@@ -30,6 +32,7 @@ def generate_score(lst1, lst2, node, current_node_visited):
     lst3 = [value for value in lst1 if value in lst2]
     return len(lst3)
 
+
 def update_visited(group):
     for i in group:
         for j in group:
@@ -37,13 +40,14 @@ def update_visited(group):
                 i.visited.append(j)
 
 
-def generate_group(nodes):
+def generate_group(nodes, output):
     new_nodes = []
-    for i in nodes:
+    num_groups = 0
+    for x in range(number_of_groups):
         current_node = nodes.pop()
         current_group = [current_node]
         visited = current_node.visited
-        for x in range(2):
+        for x in range(number_of_people_in_a_group - 1):
             current_max = -1
             node_to_add = None
             for node in nodes:
@@ -58,28 +62,39 @@ def generate_group(nodes):
             nodes.remove(node_to_add)
             update_visited(current_group)
             visited = current_node.visited + node_to_add.visited
-        print(current_group)
+        # print(current_group)
+        output.output += str(current_group) + "\n"
+        num_groups += len(current_group)
         for node in current_group:
             new_nodes.append(node)
     for new_node in new_nodes:
         nodes.append(new_node)
 
+    output.output += "\n --------------------------------- \n"
+    return num_groups
 
 
+counter = 0
+target = number_of_rounds * total_people
+
+while counter != target:
+    try:
+        counter = 0
+        nodes = []
+        output = Output()
+
+        for x in range(total_people):
+            new_node = Node(str(x))
+            nodes.append(new_node)
+        random.shuffle(nodes)
+        for x in range(number_of_rounds):
+            random.shuffle(nodes)
+            counter += generate_group(nodes, output)
+        print(counter)
+        print(output.output + " finished with " + str(number_of_rounds) + " rounds.")
+
+    except:
+        print(counter)
+        pass
 
 
-
-nodes = [A, B, C, D, E, F, G, H, I]
-random.shuffle(nodes)
-generate_group(nodes)
-print('------------------------')
-random.shuffle(nodes)
-generate_group(nodes)
-print('------------------------')
-random.shuffle(nodes)
-generate_group(nodes)
-print('------------------------')
-random.shuffle(nodes)
-generate_group(nodes)
-print('------------------------')
-print(nodes)
